4dacbfed6bb1ecd774e0b90e3fd1056fbc54bf80
Prince Codes 247 <princecodes247@gmail.com>
2021-04-19 17:29:39 -0700

    feat: update contact form

--- form.js
+++ form.js
@@ -1,16 +1,11 @@
-
-document.querySelectorAll('.form input').forEach(input => {
-  
-  input.addEventListener('blur', e => {
-   
+document.querySelectorAll(".form-item input").forEach((input) => {
+  input.addEventListener("blur", (e) => {
     if (input.value.length === 0) {
-      input.classList.remove('input-in');
+      input.classList.remove("input-in");
     }
   });
 
-  input.addEventListener('focus', e => {
-      input.classList.add('input-in');
-    
+  input.addEventListener("focus", (e) => {
+    input.classList.add("input-in");
   });
-  
 });
