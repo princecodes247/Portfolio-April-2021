aafdef14e724ec00882a1ff7fc5af40f0ebeccde
princecodes247 <princecodes247@gmail.com>
2021-06-01 00:53:36 -0700

    feat: include form support

--- form.js
+++ form.js
@@ -9,3 +9,23 @@ document.querySelectorAll(".form-item input").forEach((input) => {
     input.classList.add("input-in");
   });
 });
+
+document.querySelector("form").addEventListener("submit",()=>{
+  event.preventDefault()
+  let newForm = {
+    name: event.target.querySelector("#contact-name").value,
+    email: event.target.querySelector("#contact-email").value,
+    message: event.target.querySelector("#contact-message").value
+  }
+  console.log(newForm);
+  fetch("https://mailion.herokuapp.com/warp/send/warp222731",{
+    method: "POST",
+    body: newForm
+  }).then(()=>{
+    console.log("sent");
+    document.querySelector("form button").innerHTML = " Message Sent!"
+    setTimeout(()=>{
+      document.querySelector("form button").innerHTML = "Submit"
+    }, 2000)
+  })
+})
